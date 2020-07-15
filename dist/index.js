bc60f075b6c5f14456c8207e78a47d973b64f6ee
Eric Andrechek <ericandrechek@Erics-MBP.lan>
2020-07-15 14:54:12 -0400

    adding pretty display html

--- dist/index.js
+++ dist/index.js
@@ -16,38 +16,102 @@ class HomebridgeDisplay {
     createServer() {
         const server = http.createServer((req, res) => {
             this.log.debug('Received HTTP Path - ' + req.url);
-            let path = req.url.split('/');
-            if (path.length >= 2 ) {
-                switch (path[1])  {
-                    case "home":
-                        fs.readFile(__dirname + "/index.html")
-                        .then(contents => {
-                            res.statusCode = 200;
-                            res.setHeader("Content-Type", "text/html");
-                            res.end(contents);
-                        })
-                        .catch(err => {
-                            res.writeHead(500);
-                            res.end(err);
-                            return;
-                        });
-                        break
-                    case "callback":
-                        res.statusCode = 200;
-                        res.setHeader("Content-Type", "text/html");
-                        res.end('<html><body><h1>' + req.url + '</h1></body></html>');
-                        break
-                    default:
-                        res.writeHead(404);
-                        res.end()
-                }
+            if (req.url === "/static/background-image.png") {
+                fs.readFile(__dirname + "/static/background-image.png")
+                .then(contents => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "image/png");
+                    res.end(contents);
+                })
+                .catch(err => {
+                    res.writeHead(500);
+                    res.end(err);
+                    return;
+                });
+            } else if (req.url === "/static/main.css") {
+                fs.readFile(__dirname + "/static/main.css")
+                .then(contents => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "text/css");
+                    res.end(contents);
+                })
+                .catch(err => {
+                    res.writeHead(500);
+                    res.end(err);
+                    return;
+                });
+            } else if (req.url === "/static/black-home.png") {
+                fs.readFile(__dirname + "/static/black-home.png")
+                .then(contents => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "image/png");
+                    res.end(contents);
+                })
+                .catch(err => {
+                    res.writeHead(500);
+                    res.end(err);
+                    return;
+                });
+            } else if (req.url === "/static/placeholder.png") {
+                fs.readFile(__dirname + "/static/placeholder.png")
+                .then(contents => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "image/png");
+                    res.end(contents);
+                })
+                .catch(err => {
+                    res.writeHead(500);
+                    res.end(err);
+                    return;
+                });
+            } else if (req.url === "/static/weather-icons.min.css") {
+                fs.readFile(__dirname + "/static/weather-icons.min.css")
+                .then(contents => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "text/css");
+                    res.end(contents);
+                })
+                .catch(err => {
+                    res.writeHead(500);
+                    res.end(err);
+                    return;
+                });
+            } else if (req.url === "/static/black-home-ios.png") {
+                fs.readFile(__dirname + "/static/black-home-ios.png")
+                .then(contents => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "image/png");
+                    res.end(contents);
+                })
+                .catch(err => {
+                    res.writeHead(500);
+                    res.end(err);
+                    return;
+                });
+            } 
+            else if (req.url === "/home" || req.url === "/index.html" || req.url === "/" || req.url === "") {
+                fs.readFile(__dirname + "/index.html")
+                .then(contents => {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "text/html");
+                    res.end(contents);
+                })
+                .catch(err => {
+                    res.writeHead(500);
+                    res.end(err);
+                    return;
+                });
+            } else if (req.url === "/callback") {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "text/html");
+                res.end(req.rul);
             } else {
                 res.writeHead(404);
-                res.end()
+                res.end('404 not found')
             }
         });
-        server.listen(this.config.Config.port, () => {
-            this.log('Starting Homebridge-Display server on port ', this.config.Config.port);
+        server.listen(parseInt(this.config.Config.port), () => {
+            this.log('Starting Homebridge-Display server on port ', parseInt(this.config.Config.port));
         });
     }
 }
