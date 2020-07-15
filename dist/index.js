eed6269b039bc60b749245d20e9a3f653b99633f
Eric Andrechek <ericandrechek@Erics-MBP.lan>
2020-07-15 14:41:22 -0400

    i forgot to break the cases

--- dist/index.js
+++ dist/index.js
@@ -20,29 +20,30 @@ class HomebridgeDisplay {
             if (path.length >= 2 ) {
                 switch (path[1])  {
                     case "home":
-                        res.statusCode = 200;
-                        res.setHeader("Content-Type", "text/html");
-                        res.end('<html><body><h1>' + this.config.Config.background +  req.url + '</h1></body></html>');
+                        fs.readFile(__dirname + "/index.html")
+                        .then(contents => {
+                            res.statusCode = 200;
+                            res.setHeader("Content-Type", "text/html");
+                            res.end(contents);
+                        })
+                        .catch(err => {
+                            res.writeHead(500);
+                            res.end(err);
+                            return;
+                        });
+                        break
                     case "callback":
                         res.statusCode = 200;
                         res.setHeader("Content-Type", "text/html");
                         res.end('<html><body><h1>' + req.url + '</h1></body></html>');
+                        break
                     default:
                         res.writeHead(404);
                         res.end()
                 }
             } else {
-                fs.readFile(__dirname + "/index.html")
-                    .then(contents => {
-                        res.statusCode = 200;
-                        res.setHeader("Content-Type", "text/html");
-                        res.end(contents);
-                    })
-                    .catch(err => {
-                        res.writeHead(500);
-                        res.end(err);
-                        return;
-                    });
+                res.writeHead(404);
+                res.end()
             }
         });
         server.listen(this.config.Config.port, () => {
